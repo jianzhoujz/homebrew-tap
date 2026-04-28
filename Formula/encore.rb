e5e0daddd9c2cb85ab080601dbf02756579f1ad3
Jian Zhou <jianzhou.jz@outlook.com>
2026-04-28 18:32:24 +0800

    encore: bump to v0.3.1

--- Formula/encore.rb
+++ Formula/encore.rb
@@ -1,17 +1,17 @@
 class Encore < Formula
   desc "Encore CLI tool"
   homepage "https://github.com/jianzhoujz/encore"
-  version "1.0.0"
+  version "0.3.1"
   license "MIT"
 
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/jianzhoujz/encore/releases/download/v1.0.0/encore-1.0.0-darwin-arm64.tar.gz"
-    sha256 "d41d8cd98f00b204e9800998ecf8427e"
+    url "https://github.com/jianzhoujz/encore/releases/download/v0.3.1/encore-0.3.1-darwin-arm64.tar.gz"
+    sha256 "963aab13d219a582e5604caa1af010d341695ff37197fe3cfead8d79bc7a1ae5"
   else
-    url "https://github.com/jianzhoujz/encore/releases/download/v1.0.0/encore-1.0.0-darwin-amd64.tar.gz"
-    sha256 "d41d8cd98f00b204e9800998ecf8427e"
+    url "https://github.com/jianzhoujz/encore/releases/download/v0.3.1/encore-0.3.1-darwin-amd64.tar.gz"
+    sha256 "70370a1b958e23827e209da59a013bf73d6af0521cf44564d11a35eaf3e8aaca"
   end
 
   def install
