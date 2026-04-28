8e85463f6d5ab27795dffee3d88e0c3497dda82c
Jian Zhou <jianzhou.jz@outlook.com>
2026-04-28 15:47:45 +0800

    fix: correct version to v1.0.0 (initial release)

--- Formula/unquarantine.rb
+++ Formula/unquarantine.rb
@@ -1,17 +1,17 @@
 class Unquarantine < Formula
   desc "CLI tool to remove quarantine attributes from macOS applications"
   homepage "https://github.com/jianzhoujz/unquarantine"
-  version "1.3.0"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/jianzhoujz/unquarantine/releases/download/v1.3.0/unquarantine-1.3.0-darwin-arm64.tar.gz"
-      sha256 "5ff7b8a1cb73dace9629323ce98a62d8e6e8b0467bcdcbd7655b7b0dabfd2034"
+      url "https://github.com/jianzhoujz/unquarantine/releases/download/v1.0.0/unquarantine-1.0.0-darwin-arm64.tar.gz"
+      sha256 "4248984a3c9d09669859eb44a62a979edbe69b068537178a31b235626007c3b9"
     end
     on_intel do
-      url "https://github.com/jianzhoujz/unquarantine/releases/download/v1.3.0/unquarantine-1.3.0-darwin-amd64.tar.gz"
-      sha256 "e9484ef2469d38a18f6f079d95e7ac9936d4e0f33b400a31eacf8687faf883ee"
+      url "https://github.com/jianzhoujz/unquarantine/releases/download/v1.0.0/unquarantine-1.0.0-darwin-amd64.tar.gz"
+      sha256 "439156456c6fcc49aacd6836205c32db282bba0f441d4633ebceddddaeb36722"
     end
   end
 
