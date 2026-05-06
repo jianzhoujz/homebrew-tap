eb095a5b038ade33f11157d8c3358be00404a163
Jian Zhou <jianzhou.jz@outlook.com>
2026-05-06 14:58:56 +0800

    encore 0.3.7

--- Formula/encore.rb
+++ Formula/encore.rb
@@ -1,17 +1,17 @@
 class Encore < Formula
   desc "Local proxy for AI APIs with automatic retry"
   homepage "https://github.com/jianzhoujz/encore"
-  version "0.3.6"
+  version "0.3.7"
   license "MIT"
 
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/jianzhoujz/encore/releases/download/v0.3.6/encore-0.3.6-darwin-arm64.tar.gz"
-    sha256 "7d2366e399f7d58b64fc5f156e2c589f26a0d603f66257fafbff05af21c2a13d"
+    url "https://github.com/jianzhoujz/encore/releases/download/v0.3.7/encore-0.3.7-darwin-arm64.tar.gz"
+    sha256 "d5d24f9052944aab61b160b0dc0d1e495f8ed564ad46256fe78d04bb73713c8e"
   else
-    url "https://github.com/jianzhoujz/encore/releases/download/v0.3.6/encore-0.3.6-darwin-amd64.tar.gz"
-    sha256 "414ce368899ea262cd6e55b49ca1d642c7aa3133b53c9c9019b7be039e02edbf"
+    url "https://github.com/jianzhoujz/encore/releases/download/v0.3.7/encore-0.3.7-darwin-amd64.tar.gz"
+    sha256 "816ab6b965a1d8c8e88984d57d862b4a00362d903ddca30f1bd5c168badc7186"
   end
 
   def install
